c4c420100d2f35cf4f8dd1c01964d9fbe6296b16
sanaehururu1200 <sanaehururu1200@gmail.com>
2023-05-07 11:40:20 +0900

    commit

---
+++ web/copy.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    const messageElement = document.getElementById("message");
+
+    async function copyToClipboard(text) {
+        try {
+            await navigator.clipboard.writeText(text).then(
+                () => {
+                    /* success */
+                    var text = 'クリップボードに認証コードをコピーしました\nこのコードをワールドで入力して下さい ';
+                    if (lang != 'ja') {
+                        text = 'Copied the authentication code to the clipboard\nPlease enter this code in the world';
+                    }
+                    messageElement.innerText = text;
+                }
+            );
+        } catch (error) {
+            alert((error && error.message) || 'クリップボードへのコピーに失敗しました')
+        }
+    }
+
+    let url = new URL(window.location.href);
+    const lang = (window.navigator.languages && window.navigator.languages[0]) || window.navigator.language;
+    if (url.searchParams.get('c')) {
+        copyToClipboard(url.searchParams.get('c'));
+    } else {
+        var text = 'エラー!!';
+        if (lang != 'ja') {
+            text = 'Error!!';
+        }
+    }
+}
